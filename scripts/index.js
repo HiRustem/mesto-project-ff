e2be4d366585093f1d484cd06cafda9551b7f8c0
Rustem Hi <rustemxrid@icloud.com>
2024-01-28 14:28:45 +0300

    добавил функции создания, удаления и выведения карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const deleteCardHandler = event => event.target.parentElement.remove();
+
+const createCard = (cardName, cardLink, deleteHandler) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = cardName;
+
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.setAttribute('src', cardLink);
+  cardImage.setAttribute('alt', `Фото ${cardName}`);
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteHandler);
+
+  return cardElement
+};
+
+const initCards = () => {
+  const placesListElement = document.querySelector('.places__list');
+
+  for (let card of initialCards) {
+    const newCard = createCard(card.name, card.link, deleteCardHandler);
+    placesListElement.append(newCard);
+  }
+};
+
+initCards();
